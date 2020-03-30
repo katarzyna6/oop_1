9b096da4adaffe5e9c97980700a13ae74e505e76
unknown <katharina.r@hotmail.fr>
2020-03-30 13:44:57 +0200

    update $état

--- index.php
+++ index.php
@@ -2,46 +2,40 @@
 
 class Personnage {
 
+    //Ajouter à notre classe Personnage les caractéristiques suivantes :->santé ->le fait qu'il soit vivant ou mort
     public $nom;
     public $force;
     public $level;
-    public $sante;
-    public $etat;
+    public $health;
+    public $death;
 
+    //Faîtes en sorte que chaque personnage nous informe sur ses nouvelles propriétés grâce à sa méthode caracteristiques()
     function caracteristiques() {
-        echo $this->nom ." a une force de ". $this->force . ", une santé de ". $this->sante . ". Son niveau est: ". $this->level . ". Son état est: ". $this->etat() .".";
-    }
-    function etat() {
-        if($this->sante >= 1) {
-            $etat =  "alive";
-            } else {
-                $etat = "dead";
-        }
-        return $etat;
+        $etat = ($this->death)? "mort" : "vivant";
+        echo  $this->nom ." a une force de ". $this->force . ", une santé de ". $this->health . ". Son niveau est: ". $this->level . ". Son état est: ". $etat .".";
     }
 }
 
 $perso1 = new Personnage();
-$perso2 = new Personnage();
-$perso3 = new Personnage();
-
 $perso1->nom = "Rose";
 $perso1->force = 12;
 $perso1->level = 1;
-$perso1->sante = 3;
-$perso1->etat = 1;
+$perso1->health = 100;
+$perso1->death = false;
 
+$perso2 = new Personnage();
 $perso2->nom = "Golbu";
 $perso2->force = 15;
 $perso2->level = 1;
-$perso2->sante= 5;
-$perso2->etat = 0;
+$perso2->health= 10;
+$perso2->death = false;
 
+$perso3 = new Personnage();
 $perso3->nom = "Arthis";
 $perso3->force = 13;
 $perso3->level = 1;
-$perso3->sante = 0;
-$perso3->etat = 0;
+$perso3->health = 0;
+$perso3->death = true;
 
-$perso3->caracteristiques();
+$perso1->caracteristiques();
 
