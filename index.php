66e5d17de69aeec4773cf6d8035f2e9676fd1e77
unknown <katharina.r@hotmail.fr>
2020-03-30 14:19:45 +0200

    ajout getters/setters

--- index.php
+++ index.php
@@ -6,7 +6,7 @@ class Personnage {
     private $force;
     private $level;
 
-    function __construct($nom, $force, $level = 1) {
+    function __construct(string $nom, int $force, int $level = 1) {
        $this->nom = $nom;
        $this->force = $force;
        $this->level = $level; 
@@ -15,11 +15,31 @@ class Personnage {
     function caracteristiques() {
         echo  $this->nom ." a une force de ". $this->force .". Son niveau est: ". $this->level;
     }
+
+    function getNom(): string {
+        return $this->nom;
+    }
+
+    function setNom(string $nom) {
+        $this->nom = $nom;
+    }
+
+    function getLevel(): int {
+        return $this->level;
+    }
+
+    function setLevel(int $lvl) {
+        $this->level = $lvl;
+    }
+
 }
 
 $perso1 = new Personnage("Rose", 12);
 $perso2 = new Personnage("Golbu", 15, 2);
 $perso3 = new Personnage("Arthis", 13, 2);
+
+$perso2->setNom("Mary");
+$perso2->setLevel(2);
 
 $perso2->caracteristiques();
 
