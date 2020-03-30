cf5bfb2457cb7b03a16ac616066b2818b191dfb0
unknown <katharina.r@hotmail.fr>
2020-03-30 17:18:18 +0200

    ajout  méthode attaquer()

--- index.php
+++ index.php
@@ -8,9 +8,18 @@ class Personnage {
     private $health;
     private $death;
 
+    function __construct(string $nom, int $force, int $health = 100, int $level = 1) {
+        $this->setNom($nom);
+        $this->setForce($force);
+        $this->setHealth($health);
+        $this->setLevel($level);
+        $this->setDeath();
+        
+    }
+
     function caracteristiques() {
         $etat = ($this->death)? "mort" : "vivant";
-        echo  $this->nom ." a une force de ". $this->force . ", son état de santé est de ". $this->health ." points/100.  Son niveau est: ". $this->level .". Il est: " . $etat. ".";
+        echo  $this->nom ." a une force de ". $this->force . ", son état de santé est de ". $this->health ." points/100.  Son niveau est: ". $this->level .". Il est: " . $etat. "."."<br>";
     }
 
     function getNom(): string {
@@ -43,42 +52,36 @@ class Personnage {
 
     function setHealth(int $health) {
         $this->health = $health;
-        if($this->health < 1) {
-            $this->setDeath(true);
-        } else {
-            $this->setDeath(false);
-        }
     }
 
     function isDeath(): bool {
         return $this->death;
     }
 
-    function setDeath(int $death) {
-        $this->death = $death;
+    function setDeath() {
+        if($this->health < 1) {
+            $this->death = true;
+        } else {
+            $this->death = false;
+        }
     }
+
+    function attaquer(Personnage $perso) {
+        $perso->setHealth($perso->getHealth() - $this->force);
+        $perso->setDeath();
+    }
+
 }
 
-$perso1 = new Personnage();
-$perso1->setNom("Rose");
-$perso1->setForce(12);
-$perso1->setLevel(1);
-$perso1->setHealth(10);
-$perso1->setDeath(false);
+$perso1 = new Personnage("Rose", 12);
 
-$perso2 = new Personnage();
-$perso2->setNom("Golbu");
-$perso2->setForce(15);
-$perso2->setLevel(1);
-$perso2->setHealth(10);
-$perso2->setDeath(false);
+$perso2 = new Personnage("Golbu", 15, 10, 2);
 
-$perso3 = new Personnage();
-$perso3->setNom("Arthis");
-$perso3->setForce(13);
-$perso3->setLevel(1);
-$perso3->setHealth(0);
-$perso3->setDeath(true);
+$perso3 = new Personnage("Arthis", 13, 0);
 
-$perso1->caracteristiques();
+echo "Avant attaque : ";
+$perso2->caracteristiques();
+$perso1->attaquer($perso2);
+echo "Après l'attaque : ";
+$perso2->caracteristiques();
 
